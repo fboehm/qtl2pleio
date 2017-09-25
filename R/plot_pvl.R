4498973bc5b6d6456f82b04a158ef6bfc34cead8
Frederick Boehm <frederick.boehm@gmail.com>
2017-09-25 09:16:30 -0500

    fixed axis labels for plots

--- R/plot_pvl.R
+++ R/plot_pvl.R
@@ -8,5 +8,5 @@ plot_pvl <- function(dat){
     # plot
     ggplot2::ggplot(dat, ggplot2::aes(y = dat$lod, x = dat$marker_position, colour = dat$trace)) +
     ggplot2::geom_line() +
-    ggplot2::labs(y = "Log likelihood difference")
+    ggplot2::labs(y = "LOD", x = "marker position")
 }
