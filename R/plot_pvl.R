6d99ecae7abc296c8f06e45e1faff0ff0dc963ab
Frederick Boehm <frederick.boehm@gmail.com>
2017-09-25 20:53:07 -0500

    added vertical lines to plot function

--- R/plot_pvl.R
+++ R/plot_pvl.R
@@ -1,6 +1,8 @@
 #' Plot tidied results of a pvl scan
 #'
 #' @param dat a tibble that is the tidied output of a pvl scan
+#' @param x1 x coordinate where trait 1 has its univariate peak
+#' @param x2 x coordinate where trait 2 has its univariate peak
 #' @export
 #' @importFrom rlang .data
 
@@ -8,5 +10,7 @@ plot_pvl <- function(dat){
     # plot
     ggplot2::ggplot(dat, ggplot2::aes(y = dat$lod, x = dat$marker_position, colour = dat$trace)) +
     ggplot2::geom_line() +
-    ggplot2::labs(y = "LOD", x = "marker position")
+    ggplot2::labs(y = "LOD", x = "marker position") +
+    ggplot2::geom_vline(xintercept = x1) +
+    ggplot2::geom_vline(xintercept = x2)
 }
