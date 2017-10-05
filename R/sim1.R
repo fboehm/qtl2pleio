cf4287e30e163143114374a95fd51338e118ee82
Frederick Boehm <frederick.boehm@gmail.com>
2017-10-05 08:53:30 -0500

    updated sim1()

--- R/sim1.R
+++ R/sim1.R
@@ -1,23 +1,22 @@
 #' Simulate a single data set consisting of n subjects and 2 phenotypes for each
 #'
-#' @param X genotype probabilities matrix for a single marker
-#' @param B a matrix of allele effects
-#' @param Vg a genetic covariance matrix
-#' @param Ve an error covariance matrix
-#' @param kinship a chromosome-specific kinship matrix
+#' @param X design matrix (incorporating genotype probabilities from two loci), 2n by 2a
+#' @param B a matrix of allele effects, a rows by 2 columns
+#' @param Vg a genetic covariance matrix, 2 by 2
+#' @param Ve an error covariance matrix, 2 by 2
+#' @param kinship a chromosome-specific kinship matrix, n by n
 #' @export
 
 sim1 <- function(X, B, Vg, Ve, kinship){
-  stopifnot(nrow(X) == nrow(kinship), nrow(kinship) == ncol(kinship), nrow(Vg) == nrow(Ve),
+  stopifnot(nrow(kinship) == ncol(kinship), nrow(Vg) == nrow(Ve),
             ncol(Vg) == ncol(Ve),
             nrow(Vg) == ncol(Vg),
             nrow(Ve) == ncol(Ve)
             )
-  nrow(X) -> n_mouse
+  nrow(X) / 2 -> n_mouse
   In <- diag(n_mouse)
   Sigma <- kinship %x% Vg + In %x% Ve
   error_vec <- MASS::mvrnorm(n = 1, mu = rep(0, 2 * n_mouse), Sigma = Sigma)
-  error_mat <- matrix(data = error_vec, byrow = FALSE, ncol = 2)
-  Y <- X %*% B + error_mat
+  Y <- X %*% as.matrix(as.vector(B)) + error_vec
   return(Y)
 }
