482d8559d81423979ca3a3e9cd070e049eaf14ca
Frederick Boehm <frederick.boehm@gmail.com>
2017-10-09 11:30:59 -0500

    added expect_ statements for pvl_boot

--- tests/testthat/test-pvl_boot.R
+++ tests/testthat/test-pvl_boot.R
@@ -8,5 +8,10 @@ probs_pre <- rbinom(n = 100 * 10, size = 1, prob = 1 / 2)
 probs <- array(data = probs_pre, dim = c(100, 1, 10))
 
 X <- as.matrix(probs[ , , 5])
+nboot <- 5
 
-pvl_boot(X = X, B = c(1, 2), Vg_initial = diag(2), Ve_initial = diag(2), kinship = diag(100), probs = probs, start_snp = 1, n_snp = 10, nboot = 5)
+test_that("pvl_boot returns a vector of length nboot", {
+  expect_length(pvl_boot(X = X, B = c(1, 2), Vg_initial = diag(2), Ve_initial = diag(2), kinship = diag(100), probs = probs, start_snp = 1, n_snp = 10, nboot = nboot), nboot)
+  expect_true(is.numeric(pvl_boot(X = X, B = c(1, 2), Vg_initial = diag(2), Ve_initial = diag(2), kinship = diag(100), probs = probs, start_snp = 1, n_snp = 10, nboot = nboot)))
+  expect_gte(pvl_boot(X = X, B = c(1, 2), Vg_initial = diag(2), Ve_initial = diag(2), kinship = diag(100), probs = probs, start_snp = 1, n_snp = 10, nboot = nboot)[1], 0)
+})
