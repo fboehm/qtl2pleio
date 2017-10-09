d28c666c8210854b01f50fe3ac9ea6c7cb6084b1
Frederick Boehm <frederick.boehm@gmail.com>
2017-10-09 11:00:10 -0500

    fixed call to sim1 within pvl_boot()

---
+++ tests/testthat/test-pvl_boot.R
@@ -0,0 +1,12 @@
+library(qtl2pleio)
+
+context("testing parametric bootstrap code")
+
+
+
+probs_pre <- rbinom(n = 100 * 10, size = 1, prob = 1 / 2)
+probs <- array(data = probs_pre, dim = c(100, 1, 10))
+
+X <- probs[ , , 5]
+
+pvl_boot(X = X, B = c(1, 2), Vg_initial = diag(2), Ve_initial = diag(2), kinship = diag(100), probs = probs, start_snp = 1, n_snp = 10, nboot = 5)
