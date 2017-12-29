90cf56755ba4785555ae4d9f91b5c1d51d3b5461
Frederick Boehm <frederick.boehm@gmail.com>
2017-12-29 11:11:41 -0600

    updated docs

--- tests/testthat/test-id_recombination.R
+++ tests/testthat/test-id_recombination.R
@@ -2,6 +2,6 @@ library(qtl2pleio)
 context("testing id_recombination")
 
 test_that("logicals work as expected", {
-  expect_true(id_recombination(c(0, 1), c(0, 1)),
-  expect_false(id_recombination(c(1, 0), c(0, 1))))
+  expect_true(id_recombination(),
+  expect_false(probs_equal(c(1, 0), c(0, 1))))
 })
