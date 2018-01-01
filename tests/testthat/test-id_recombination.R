449b42029fa1817d7c84e29294d6b652b3653bba
Frederick Boehm <frederick.boehm@gmail.com>
2017-12-31 20:24:31 -0600

    added call to round() within probs_equal()

--- tests/testthat/test-id_recombination.R
+++ tests/testthat/test-id_recombination.R
@@ -1,12 +1,12 @@
 library(qtl2pleio)
-context("testing id_recombination")
+context("testing id_recombination in simple cases")
 
 
 probs1 <- cbind(c(1, 1, 1, 0, 0, 0), c(0, 0, 0, 1, 1, 1))
 probs2 <- cbind(c(1, 1, 1), c(0, 0, 0))
 
 
-test_that("logicals work as expected", {
+test_that("simple cases work as expected", {
   expect_true(id_recombination(probs1))
   expect_false(id_recombination(probs2))
 })
