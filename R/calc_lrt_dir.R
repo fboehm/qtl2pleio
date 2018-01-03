38119380196a92451820323f01c5918202369eff
Frederick Boehm <frederick.boehm@gmail.com>
2018-01-03 12:01:26 -0600

    added calc_lrt_dir function

---
+++ R/calc_lrt_dir.R
@@ -0,0 +1,16 @@
+#' Calculate likelihood ratio tests for all files in a single directory. Each file contains output of a two-dimensional scan.
+#'
+#' @param directory path to directory that contains the collection of files
+#' @export
+
+calc_lrt_dir <- function(directory){
+  dir(directory) -> fns
+  lrt <- numeric(length = 400)
+  i <- 1
+  for (fn in fns){
+    read.table(file.path(PVL_DIR, fn)) -> scan_out
+    calc_lrt(as.matrix(scan_out)) -> lrt[i]
+    i <- i + 1
+  }
+  return(lrt)
+}
