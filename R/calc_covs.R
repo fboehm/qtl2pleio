056e83aaf93e02dd34c5124a728e7cb19b98b769
Frederick Boehm <frederick.boehm@gmail.com>
2018-01-13 14:36:03 -0600

    updated calc_covs R code & doc

--- R/calc_covs.R
+++ R/calc_covs.R
@@ -1,6 +1,6 @@
 #' Calculate Vg and Ve from Y and kinship
 #'
-#' @param pheno n by 2 matrix of phenotypes
+#' @param pheno n by d matrix of phenotypes
 #' @param kinship a kinship matrix, n by n
 #' @param X1pre n by c design matrix. c = 1 to ignore genotypes
 #' @export
@@ -11,8 +11,9 @@ calc_covs <- function(pheno, kinship, X1pre = rep(1, nrow(kinship))){
   n_mouse <- nrow(kinship)
   X1 <- t(X1pre) %*% U
   Y <- t(pheno) %*% U
+  ncol(pheno) -> d
   # run MphEM with only a design matrix that contains only the intercept term (and not any genotype info)
-  foo <- gemma2::MphEM(X = X1, Y = Y, eval = eval, V_g = diag(2), V_e = diag(2))
+  foo <- gemma2::MphEM(X = X1, Y = Y, eval = eval, V_g = diag(d), V_e = diag(d))
   Vg <- foo[[length(foo)]][[2]]
   Ve <- foo[[length(foo)]][[3]]
   out <- list(Vg = Vg, Ve = Ve)
