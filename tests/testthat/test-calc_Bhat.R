17764cdc5ce3de61966b9d7f23751ae548289f14
Frederick Boehm <frederick.boehm@gmail.com>
2018-02-01 11:41:24 -0600

    fixed typo

--- tests/testthat/test-calc_Bhat.R
+++ tests/testthat/test-calc_Bhat.R
@@ -4,12 +4,12 @@ context("Check calc_Bhat")
 # setup
 X1 <- as.matrix(rbinom(n = 100, size = 1, prob = 1 / 2))
 X <- gemma2::stagger_mats(X1, X1)
-Sigma <- matrix(data = rep(1, 200 * 200), nrow = 200)
+Sigma_inv <- matrix(data = rep(1, 200 * 200), nrow = 200)
 Y <- runif(200)
 
 
-test_that("error is returned when inputted Sigma is not positive definite", {
-  expect_error(calc_Bhat(X = X, Sigma_inv = Sigma, Y = Y))
+test_that("error is returned when inputted Sigma_inv is not positive definite", {
+  expect_error(calc_Bhat(X = X, Sigma_inv = Sigma_inv, Y = Y))
 })
 test_that("dimensions of returned object are correct", {
   expect_equal(nrow(calc_Bhat(X = X, Sigma_inv = diag(200), Y = Y)), 2)
