6660c25d906998524a53fec1507680740c7d7457
Frederick Boehm <frederick.boehm@gmail.com>
2018-02-01 11:31:11 -0600

    fixed typo

--- R/calc_Bhat.R
+++ R/calc_Bhat.R
@@ -3,7 +3,7 @@
 #' @param X dn by df block-diagonal design matrix that incorporates genetic info for two markers. Note that we can use the same marker data twice.
 #' @param Sigma_inv dn by dn inverse covariance matrix, often composed as the inverse of \eqn{K \otimes V_g + I_n \otimes V_e}
 #' @param Y dn by 1 matrix, ie, a column vector, of d phenotypes' measurements
-#' @return a dn by 1 matrix of GLS-estimated allele effects
+#' @return a df by 1 matrix of GLS-estimated allele effects
 #' @examples
 #' X1 <- as.matrix(rbinom(n = 100, size = 1, prob = 1 / 2))
 #' X <- gemma2::stagger_mats(X1, X1)
