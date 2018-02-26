caad32b51a73807d841de167b86372c2f05de58e
Frederick Boehm <frederick.boehm@gmail.com>
2018-02-25 19:49:08 -0600

    fixed function & updated docs

--- R/calc_lrt_tib.R
+++ R/calc_lrt_tib.R
@@ -1,7 +1,7 @@
 #' Calculate a LRT statistic from the (new) output from scan_pvl
 #'
-#' @param tib
-#' @example
+#' @param tib outputted tibble from scan_pvl
+#' @examples
 #' rep(paste0("Marker", 1:3), times = 3) -> marker1
 #' rep(paste0("Marker", 1:3), each = 3) -> marker2
 #' runif(9, -1, 0) -> ll
@@ -11,8 +11,9 @@
 #' @return a number, the LRT statistic
 calc_lrt_tib <- function(tib){
   ncol(tib) -> nc
+  as.matrix(tib[ , - nc]) -> smat
   # define an indicator vector for which rows to keep when making pleio_tib
-  pleio_ind <- apply(FUN = function(x) identical(x, rep(x[1], nc - 1)), X = tib[ , - nc], MARGIN = 1)
+  pleio_ind <- apply(FUN = function(x) {names(x) <- NULL; identical(x, rep(x[1], nc - 1))}, X = smat, MARGIN = 1)
   pleio_tib <- tib[pleio_ind, ]
   return(max(tib$ll) - max(pleio_tib$ll))
 }
