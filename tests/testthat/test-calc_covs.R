fd73eec817f97c7c70d185cedc92e8102da56273
Frederick Boehm <frederick.boehm@gmail.com>
2018-03-03 12:32:54 -0600

    updated tests for calc_covs

--- tests/testthat/test-calc_covs.R
+++ tests/testthat/test-calc_covs.R
@@ -9,7 +9,7 @@ calc_covs(phe, kinship = diag(100)) -> cc1
 calc_covs(phe, kinship = diag(100), covariates = sex) -> cc2
 
 phe3 <- matrix(runif(300), ncol = 3)
-phe5 <- matrix(runif(500), ncol = 3)
+phe5 <- matrix(runif(500), ncol = 5)
 
 
 test_that("calc_covs returns different answers when covariates are used vs. not used", {
@@ -21,4 +21,5 @@ test_that("calc_covs returns different answers when covariates are used vs. not 
 test_that("calc_covs accommodates d-variate phenotype for d more than 2", {
   expect_equal(calc_covs(phe3, diag(100))$Vg, calc_covs(phe3, diag(100))$Ve)
   expect_equal(calc_covs(phe5, diag(100))$Vg, calc_covs(phe5, diag(100))$Ve)
+  expect_equal(calc_covs(phe5, diag(100), covariates = sex)$Vg, calc_covs(phe5, diag(100), covariates = sex)$Ve)
 })
