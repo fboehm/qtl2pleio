60eeca5a575a2918e37fe3cbdc9f8cbc5163c3e3
Frederick Boehm <frederick.boehm@gmail.com>
2018-03-02 20:56:28 -0600

    added test to ensure that Vg and Ve are equal for 5-variate phenotype and kinship = identity.

--- tests/testthat/test-calc_covs.R
+++ tests/testthat/test-calc_covs.R
@@ -9,6 +9,7 @@ calc_covs(phe, kinship = diag(100)) -> cc1
 calc_covs(phe, kinship = diag(100), covariates = sex) -> cc2
 
 phe3 <- matrix(runif(300), ncol = 3)
+phe5 <- matrix(runif(500), ncol = 3)
 
 
 test_that("calc_covs returns different answers when covariates are used vs. not used", {
@@ -19,4 +20,5 @@ test_that("calc_covs returns different answers when covariates are used vs. not 
 
 test_that("calc_covs accommodates d-variate phenotype for d more than 2", {
   expect_equal(calc_covs(phe3, diag(100))$Vg, calc_covs(phe3, diag(100))$Ve)
+  expect_equal(calc_covs(phe5, diag(100))$Vg, calc_covs(phe5, diag(100))$Ve)
 })
