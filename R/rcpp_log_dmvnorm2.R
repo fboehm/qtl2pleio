74689eca5c1dee171be310ce0a4d685cd8c14237
Frederick Boehm <frederick.boehm@gmail.com>
2018-06-04 16:00:20 -0500

    fixed spacing in code

--- R/rcpp_log_dmvnorm2.R
+++ R/rcpp_log_dmvnorm2.R
@@ -15,5 +15,5 @@ rcpp_log_dmvnorm2 <- function(inv_S, mu, x, S)
   )
   n <- length(x)
   diag <- rcppeigen_get_diag(S)
-  return((-n/2)*log(2*pi)-sum(log(diag))-(1/2)*(x-mu)%*%inv_S%*%(x-mu))
+  return((- n / 2) * log(2 * pi) - sum(log(diag)) - (1 / 2) * (x - mu) %*% inv_S %*% (x - mu))
 }
