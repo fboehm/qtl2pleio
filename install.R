0519aef7469ffea3530726dc775c126e23a5e3ec
Frederick Boehm <frederick.boehm@gmail.com>
2019-02-25 12:55:59 -0600

    added install.R and runtime.txt for binder

---
+++ install.R
@@ -0,0 +1,14 @@
+cran_pkgs<- c("MASS",
+"microbenchmark",
+"mvtnorm",
+"progress",
+"Rcpp",
+"RcppEigen",
+"rlang",
+"parallel",
+"broman",
+"devtools")
+install.packages(cran_pkgs)
+devtools::install_github("rqtl/qtl2@b5785d5")
+devtools::install_github("fboehm/gemma2@2872396")
+devtools::install_github("fboehm/qtl2pleio@df0ffc3")
