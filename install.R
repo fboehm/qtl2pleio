62338195f386c96ea6c92b47aac2fbaf13730569
Frederick Boehm <frederick.boehm@gmail.com>
2019-02-25 15:17:49 -0600

    added rmarkdown to install.R

--- install.R
+++ install.R
@@ -7,7 +7,8 @@ cran_pkgs<- c("MASS",
 "rlang",
 "parallel",
 "broman",
-"devtools")
+"devtools",
+"rmarkdown")
 install.packages(cran_pkgs)
 devtools::install_github("rqtl/qtl2@b5785d5")
 devtools::install_github("fboehm/gemma2@2872396")
