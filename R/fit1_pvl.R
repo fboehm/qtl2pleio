51602b0d4c43198f7fbfb495540f4a09dc874705
Frederick J. Boehm <frederick.boehm@gmail.com>
2019-10-08 07:28:17 -0400

    updated fit1_pvl

--- R/fit1_pvl.R
+++ R/fit1_pvl.R
@@ -7,7 +7,7 @@
 #'
 #' @param indices a vector of indices for extracting elements of `probs` array
 #' @param start_snp an integer to specify the index of the marker where the scan - in call to scan_pvl - starts. This argument is needed because `mytab` has only relative indices (relative to the `start_snp` marker)
-#' @param probs founder allele probabilities matrix
+#' @param probs founder allele probabilities array
 #' @param addcovar additive covariates matrix
 #' @param inv_S inverse covariance matrix for the vectorized phenotype
 #' @param S covariance matrix for the vectorized phenotype, ie, the inverse of inv_S
@@ -15,30 +15,18 @@
 #' @export
 #' @return a number, the log-likelihood for the specified model
 #' @examples
-#' # read data
-#' iron <- qtl2::read_cross2(system.file("extdata", "iron.zip", package="qtl2"))
-#' # insert pseudomarkers into map
-#' map <- qtl2::insert_pseudomarkers(iron$gmap, step=1)
-#' # calculate genotype probabilities
-#' probs <- qtl2::calc_genoprob(iron, map, error_prob=0.002)
-#' # grab phenotypes and covariates; ensure that covariates have names attribute
-#' pheno <- iron$pheno
-#' # leave-one-chromosome-out kinship matrices
-#' kinship <- qtl2::calc_kinship(probs, "loco")
-#' # get founder allele probabilites
-#' probs <- qtl2::genoprob_to_alleleprob(probs)
-#' cc_out <- calc_covs(pheno, kinship$`1`, max_iter = 10000, max_prec = 1e-07, covariates = NULL)
-#' Vg <- cc_out$Vg
-#' Ve <- cc_out$Ve
-#' # define Sigma
-#' Sigma <- calc_Sigma(Vg, Ve, kinship$`1`)
-#' # define Sigma_inv
+#' n <- 50
+#' pheno <- matrix(rnorm(2 * n), ncol = 2)
+#' Vg <- diag(2)
+#' Ve <- diag(2)
+#' Sigma <- calc_Sigma(Vg, Ve, diag(n))
 #' Sigma_inv <- solve(Sigma)
-#' # prepare table of marker indices for each call of scan_pvl
-#' mytab <- prep_mytab(d_size = 2, n_snp = 10)
-#' # set up parallel analysis
+#' probs <- array(dim = c(n, 2, 5))
+#' probs[ , 1, ] <- rbinom(n * 5, size = 1, prob = 0.2)
+#' probs[ , 2, ] <- 1 - probs[ , 1, ]
+#' mytab <- prep_mytab(d_size = 2, n_snp = 5)
 #' fit1_pvl(mytab[1, ], start_snp = 1,
-#' probs = probs$`1`, addcovar = NULL, inv_S = Sigma_inv,
+#' probs = probs, addcovar = NULL, inv_S = Sigma_inv,
 #' S = Sigma,
 #' pheno = pheno
 #' )
