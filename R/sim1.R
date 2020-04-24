1411870013c17a10b5248e8c9890b0e70a1200ea
Frederick J. Boehm <frederick.boehm@gmail.com>
2020-04-24 18:09:06 -0400

    removed as.matrix from sim1 code; added example for sim1

--- R/sim1.R
+++ R/sim1.R
@@ -6,6 +6,14 @@
 #' @param Ve an error covariance matrix, d by d
 #' @param kinship a chromosome-specific kinship matrix, n by n
 #' @export
+#' @examples
+#' n_mouse <- 20
+#' geno <- rbinom(n = n_mouse, size = 1, prob = 1 / 2)
+#' X <- gemma2::stagger_mats(geno, geno)
+#' B <- matrix(c(1, 2), ncol = 2, nrow = 1)
+#' Vg <- diag(2)
+#' Ve <- diag(2)
+#' sim1(X, B, Vg, Ve, kinship = diag(n_mouse))
 
 sim1 <- function(X, B, Vg, Ve, kinship) {
     stopifnot(nrow(kinship) == ncol(kinship), nrow(Vg) == nrow(Ve), ncol(Vg) == ncol(Ve), nrow(Vg) ==
@@ -14,6 +22,6 @@ sim1 <- function(X, B, Vg, Ve, kinship) {
     Sigma <- calc_Sigma(Vg, Ve, kinship)
     n_mouse <- nrow(kinship)
     error_vec <- MASS::mvrnorm(n = 1, mu = rep(0, d_size * n_mouse), Sigma = Sigma)
-    Y <- X %*% as.matrix(as.vector(B)) + error_vec
+    Y <- X %*% as.vector(B) + error_vec
     return(Y)
 }
